79431f50349ad119311eb59cbcb8a84689494b54
Aqua1013 <57598565+Aqua1013@users.noreply.github.com>
2022-06-17 01:16:48 +0530

    code cleanups & optimizations

--- Sorts.java
+++ Sorts.java
@@ -1,34 +1,33 @@
 import java.util.Random;
 
 public class Sorts {
-
+    
     public static int[] bogo(int[] arr) {
-      
+        // declare vars
         Random random = new Random();
         boolean sorted = false;
         int randIndex = 0;
         int temp = 0;
-      
+        
         while(!sorted){
-          
-          // randomize arr
+            // randomize arr
             for(int i = 0; i < arr.length; i++){
                 randIndex = random.nextInt(arr.length - 1);
                 temp = arr[randIndex];
                 arr[randIndex] = arr[i];
                 arr[i] = temp;
             }
-          
-          //check if sorted
-            boolean yes = true;
-            for(int n = 1; n < arr.length; n++) {
-                if(arr[n] < arr[n-1]){yes = false; break;}
+            // check if sorted
+            check : {
+                for(int n = 1; n < arr.length; n++)
+                    if(arr[n] < arr[n-1])
+                        break check;
+                
+                sorted = true;
             }
-            if(yes) sorted = true;
-          
         }
-      
+        
         return arr;
-    }
+    } // end bogo()
 
-}
+} // end class
